1eb9c2d4f1d5af0810f485d8b7722b9be84fd027
s.k <iskyteamnso@gmail.com>
2025-02-03 15:46:36 +0100

    wip: add/remove rings

--- internal/repository/user_ring.go
+++ internal/repository/user_ring.go
@@ -1,6 +1,7 @@
 package repository
 
 import (
+	"errors"
 	"oauth2-server/internal/dependency"
 	"oauth2-server/internal/models"
 )
@@ -14,9 +15,14 @@ func (repo *PostgresDB) UserRingRepository() dependency.UserRingRepository {
 }
 
 func (repo *PostgresDB) SaveUserRing(ur *models.UserRing) error {
-	if err := repo.db.Create(ur).Error; err != nil {
-		return err
+	save := models.UserRing{}
+	result := repo.db.Where("ring_id = ? AND user_id = ?", ur.RingID, ur.UserID).First(&save)
+	if result.RowsAffected == 0 {
+		if err := repo.db.Create(&ur).Error; err != nil {
+			return errors.New("error save ring: " + err.Error())
+		}
 	}
+
 	return nil
 }
 
