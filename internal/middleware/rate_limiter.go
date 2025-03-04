3b57f77af4df417632daf7a281d98586dc2f7d17
s.k <iskyteamnso@gmail.com>
2025-03-04 10:14:30 +0100

    v0.0.24: update rate limiter

--- internal/middleware/rate_limiter.go
+++ internal/middleware/rate_limiter.go
@@ -1,19 +1,39 @@
 package middleware
 
 import (
-	"golang.org/x/time/rate"
 	"net/http"
+	"sync"
+
+	"golang.org/x/time/rate"
 )
 
-var limiter = rate.NewLimiter(1, 5) // 1 запрос в секунду, максимум 5 в очереди
+var (
+	limiters = make(map[string]*rate.Limiter)
+	mu       sync.Mutex
+)
 
-// RateLimiter
+func getLimiter(key string) *rate.Limiter {
+	mu.Lock()
+	defer mu.Unlock()
+
+	limiter, exists := limiters[key]
+	if !exists {
+		limiter = rate.NewLimiter(100, 100)
+		limiters[key] = limiter
+	}
+	return limiter
+}
+
 func RateLimiter(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		userKey := r.RemoteAddr
+		limiter := getLimiter(userKey)
+
 		if !limiter.Allow() {
 			http.Error(w, "Too many requests", http.StatusTooManyRequests)
 			return
 		}
+
 		next.ServeHTTP(w, r)
 	})
 }
